5c754454bea653aff8194599753cc480639f761f
Jeff Shepherd <jeffindenver2002@yahoo.com>
2020-07-22 17:34:41 -0500

    added assignment 3.3

---
+++ week-3/secure-profile-app/src/app/app.routing.ts
@@ -0,0 +1,17 @@
+//author: Professor Krasso
+//modified by: Jeff Shepherd
+
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SigninComponent } from './signin/signin.component';
+
+export const AppRoutes: Routes = [
+  {
+    path: '',
+    component: SigninComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  }
+]
---
+++ week-3/secure-profile-app/src/app/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  isLoggedIn: boolean;
+  assignment: string = "Excercise 2.3 - Data Binding";
+
+  constructor(private route: ActivatedRoute) {
+    this.isLoggedIn = Boolean(this.route.snapshot.queryParamMap.get('isLoggedIn'));
+  }
+
+  ngOnInit(): void {
+  }
+
+}
